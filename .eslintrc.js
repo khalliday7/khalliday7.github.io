25231f98c38b98de6e0b11047c7dd942cb4f49a0
Kevin Halliday <halliday.kevin7@gmail.com>
2020-05-12 14:53:08 -0400

    Fix broken linting configuration.
    Some required packages were not installed. Some rules were too strict
    for my liking.

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+/**
+ * Eslint configuration.
+ */
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    jsx: true,
+    useJSXTextNode: true,
+    sourceType: 'module',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
